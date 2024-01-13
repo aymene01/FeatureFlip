efe161851df0afcc31baa2edcf9e623b4823ba84
aymene01 <aymene01@hotmail.fr>
2024-01-13 22:06:12 +0100

    fix sign in page

--- apps/web/src/app/(auth)/signin/page.tsx
+++ apps/web/src/app/(auth)/signin/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { Button } from '@feature-flip/ui'
-import { signIn } from 'next-auth/react'
+import { signIn } from '@feature-flip/auth/react'
 import { FcGoogle } from 'react-icons/fc'
 
 function SignIn() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-50 to-blue-100">
       <div className="p-6 rounded-lg bg-white shadow-lg flex flex-col space-y-4 text-center">
-        <h1 className="text-2xl text-gray-800 font-light">Welcome to the Inno Feature Flag Platform</h1>
+        <h1 className="text-2xl text-gray-800 font-light">Welcome to FeatureFlip Platform</h1>
         <p className="text-gray-500">Streamline your workflow with a single click</p>
         <div className="pt-4">
           <div className="inline-block">
--- apps/web/src/app/(dashboard)/page.tsx
+++ apps/web/src/app/(dashboard)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center space-y-3">
-      <h1 className="font-extralight text-4xl">Inno Feature Flag Platform</h1>
+      <h1 className="font-extralight text-4xl">FeatureFlip</h1>
       <p className="text-gray-500">Welcome {session?.user?.name}</p>
       {session && session.user && <Button onClick={() => signOut()}>Sign out</Button>}
     </div>
